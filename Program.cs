5239dd7ce39863febdc099a9c6c7b6845d38aae7
Sebastian <Sebastian@DESKTOP-FGHKP6U>
2023-04-13 17:54:57 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PhoneBook App 1.0");
+
+            Console.WriteLine("1 Add contact");
+            Console.WriteLine("2 Display contact by number");
+            Console.WriteLine("3 Display all contacts");
+            Console.WriteLine("4 Search contacts");
+            Console.WriteLine("PRESS X TO CLOSE THE APP");
+
+            var userInput = Console.ReadLine();
+            var phoneBook = new PhoneBook();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Insert number");
+                        var number = Console.ReadLine();
+                        Console.WriteLine("Insert name");
+                        var name = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+
+                        phoneBook.AddContact(newContact);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Insert number");
+                        var numberToSearch = Console.ReadLine();
+
+                        phoneBook.DisplayContact(numberToSearch);
+                        break;
+
+                    case "3":
+                        phoneBook.DisplayAllContacts();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Insert search phrase");
+                        var searchPhrase = Console.ReadLine();
+
+                        phoneBook.DisplayMatchingContacts(searchPhrase);
+                        break;
+
+                    case "x":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid number");
+                        break;
+                }
+                Console.WriteLine("Select operation");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
